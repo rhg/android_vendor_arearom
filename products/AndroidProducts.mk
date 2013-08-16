b213dc949daf286ef6d0ad6d72f8aa1dad1fbcfb
Ricardo Gomez <ricardo.gomez331@gmail.com>
2013-08-15 23:50:24 -0500

    Initial Vendor files

---
+++ products/AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/grouper.mk
---
+++ products/grouper.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/asus/grouper/slim.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := grouper
+PRODUCT_NAME := arearom_grouper
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := Asus
